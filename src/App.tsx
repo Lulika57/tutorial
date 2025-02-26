e55217b927cc539e9a623d55f2e07faab2242880
Lulika57 <kharchenko.anastasya2013@gmail.com>
2025-02-26 15:54:12 -0600

    finished list of provinces and cities

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,29 @@ function App() {
 
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
-    function handleClick() {
+    function handleClick(index: number) {
         const somethingIsAlreadyClicked = selectedIndex >= 0;
-        if (somethingIsAlreadyClicked) {
-            setSelectedIndex(-1)
-        }
-        else {
+        const isClickedAlreadyOpened = index == selectedIndex;
 
+        if (somethingIsAlreadyClicked && isClickedAlreadyOpened) {
+            // everything should be closed
+            setSelectedIndex(-1)
+        } else {
+            // open 
+            setSelectedIndex(index)
         }
     }
 
     return (
         <div className="w-full flex flex-col">
             {listOfProvinces.map((province, index) => (
-                <>
-                    <p onClick={handleClick} key={index}>{province}</p>
-                    {selectedIndex === index && <p>
+                <div onClick={(event) => handleClick(index)}>
+                    <p key={index}>{province}</p>
+                    {selectedIndex == index && <p>
                         the capital of {listOfProvinces[selectedIndex]} is {listOfCities[selectedIndex]}
                     </p>}
                     <hr />
-                </>
+                </div>
             ))}
 
         </div>
