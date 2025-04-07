7d179ceb313bbcc3652031a26d17e0a4264eab7c
Dane <info.danewanke@gmail.com>
2025-04-06 21:18:46 -0500

    a final version so far - send to my mom

--- src/recipeApp.tsx
+++ src/recipeApp.tsx
@@ -31,14 +31,16 @@ function App() {
 
     return (
         <div className="size-screen overflow-hidden">
-            <div className='flex h-[80vh]'>
-                <div className="w-[40%] max-h-[80vh] overflow-auto custom-scrollbar">
-                    <p className='font-amatic font-bold text-5xl text-green-950'>🧁 Nastya's Favourite Muffins 🧁</p>
+            <h1 className='font-amatic font-bold text-5xl text-green-950 w-full text-center py-6'>
+                <span className='text-[40px]'>🧁🧁🧁🧁🧁🧁🧁🧁🧁🧁🧁</span> Ana's Favourite Muffins <span className='text-[40px]'>🧁🧁🧁🧁🧁🧁🧁🧁🧁🧁🧁🧁</span></h1>
+            <div className='flex h-[65vh]'>
+                <div className="mx-4 flex flex-col justify-center font-amatic w-[30%] max-h-[80vh] overflow-auto custom-scrollbar">
+                    {/* <p className='font-amatic font-bold text-5xl text-green-950'>🧁 Nastya's Favourite Muffins 🧁</p> */}
                     {listOfIngridients.map((ingridient, index) => (
                         <div
                             onClick={() => handleClick(index)}
                             className={cn(
-                                'shadow-green-50 px-2.5 py-3 text-2xl text-white',
+                                'shadow-green-50 px-2.5 py-3 text-4xl text-white rounded-2xl',
                                 isEven(index) ? 'bg-green-800' : 'bg-pink-200'
                             )}
                         >
@@ -64,16 +66,28 @@ function App() {
 
                     ))}
                 </div>
-                <div className='w-[60%] px-4 flex flex-col justify-end gap-5'>
-                    <p className='bg-green-50 py-2 text-justify font-yellowtail'>1. Mix all the wet ingredients and sugar together using a mixer with dough extentions or a whisk (note: whisk helps you to build your muscsles). It would be better if you have them ready room temperature. But make sure you don't overheat them if you use a microwave to warm them up because they won't work properly this way. You can warm up butter in two ways: either melt it on a stove but don't let it boil - when you see a few pieces left just remove from the heat stirring all the time; or take butther out of the fridge and let it sit for an hour at least. From my personal experience using soft butter makes muffins fluffier (but this way a mixer is a must have).</p>
-                    <p className='bg-green-50 text-justify font-yellowtail'>2. Add dry ingredients - flour, vanilla and baking soda. To get rid of soda taste just pour a little bit vinegar on a spoon, make use soda is covered with bubbles. It creates a simple chemical reactin so now you're not just a baker - you're a chemist as well - watch it fizzy and bubbly.</p>
+                <div className='w-[70%] px-4 flex flex-col justify-start gap-5  '>
+                    <p className='bg-green-50 py-2 text-justify font-yellowtail text-[20px]'>1. Mix all the wet ingredients and sugar together using a mixer with dough extentions or a whisk (note: whisk helps you to build your muscsles). It would be better if you have them ready room temperature. But make sure you don't overheat them if you use a microwave to warm them up because they won't work properly this way. You can warm up butter in two ways: either melt it on a stove but don't let it boil - when you see a few pieces left just remove from the heat stirring all the time; or take butther out of the fridge and let it sit for an hour at least. From my personal experience using soft butter makes muffins fluffier (but this way a mixer is a must have).</p>
+                    <p className='bg-green-50 text-justify font-yellowtail text-[20px]'>2. Add dry ingredients - flour, vanilla and baking soda. To get rid of soda taste just pour a little bit vinegar on a spoon, make use soda is covered with bubbles. It creates a simple chemical reactin so now you're not just a baker - you're a chemist as well - watch it fizzy and bubbly.</p>
 
-                    <div className='bg-green-50 py-2'>
-                        <p className='text-3xl underline py-1'>Substitutes:</p>
-                        <p><span className='font-bold'>KEFIR</span> - Plain Yogurt, Buttermilk.</p>
-                        <p><span className='font-bold'>BUTTER</span> - Margarine (125 g).</p>
-                        <p><span className='font-bold'>VINEGAR</span> - Lemon or Lime Juice.</p>
-                        <p><span className='font-bold'>VANILLA SUGAR</span> - Vanilla Extract.</p>
+                    <div className='py-2'>
+                        <p className='text-3xl underline py-1 font-yellowtail'>Substitutes:</p>
+                        <p><span className='font-bold'>
+                            KEFIR</span> 
+                            <span className='font-yellowtail text-2xl'>- Plain Yogurt, Buttermilk.</span>
+                            </p>
+                        <p><span className='font-bold'>
+                            BUTTER</span> 
+                            <span className='font-yellowtail text-2xl'>- Margarine (125 g).</span>
+                            </p>
+                        <p><span className='font-bold'>
+                            VINEGAR</span>
+                            <span className='font-yellowtail text-2xl'>- Lemon or Lime Juice.</span>
+                            </p>
+                        <p><span className='font-bold'>
+                            VANILLA SUGAR</span> 
+                            <span className='font-yellowtail text-2xl'>- Vanilla Extract.</span>
+                            </p>
                         {/* // span is like a div but it doesn't create a new line */}
                     </div>
                 </div>
