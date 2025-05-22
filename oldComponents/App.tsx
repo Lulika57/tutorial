515d5b9a4a0bd156a57d95c7953d7f6881c8e3a5
Dane <info.danewanke@gmail.com>
2025-05-21 19:09:05 -0500

    cleanup

---
+++ oldComponents/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+// import cn from 'clsx';
+import './App.css';
+import { ExampleButton } from './ExampleButton';
+import { ExampleAlert } from './components/ExampleAlert';
+
+
+
+function App() {
+
+    const [alertVisible, setAlertVisibility] = useState(false);
+
+    return (
+        <div className="w-full">
+            {alertVisible && <ExampleAlert onClose={() => setAlertVisibility(false)}></ExampleAlert>}
+            <div className="static size-screen">
+                <div className="flex flex-col justify-center">
+                    <div className="w-full flex justify-center">
+                        <ExampleButton onClick={() => setAlertVisibility(true)}></ExampleButton>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
+
--- src/components/SlideShowMarquee.tsx
+++ src/components/SlideShowMarquee.tsx
@@ -1,13 +1,13 @@
 import Marquee from "react-fast-marquee";
 
-import muffinPic from './assets/1.jpg';
-import muffinPic2 from './assets/2.jpg';
-import muffinPic3 from './assets/3.jpg';
-import muffinPic4 from './assets/4.jpg';
-import muffinPic5 from './assets/5.jpg';
-import muffinPic6 from './assets/6.jpg';
-import muffinPic7 from './assets/7.jpg';
-import muffinPic8 from './assets/8.jpg';
+import muffinPic from '../assets/1.jpg';
+import muffinPic2 from '../assets/2.jpg';
+import muffinPic3 from '../assets/3.jpg';
+import muffinPic4 from '../assets/4.jpg';
+import muffinPic5 from '../assets/5.jpg';
+import muffinPic6 from '../assets/6.jpg';
+import muffinPic7 from '../assets/7.jpg';
+import muffinPic8 from '../assets/8.jpg';
 
 const muffinPics = [
     muffinPic, muffinPic2, muffinPic3, muffinPic4, muffinPic5, muffinPic6, muffinPic7, muffinPic8
